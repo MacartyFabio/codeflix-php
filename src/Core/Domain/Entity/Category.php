0a75e8b24044d675fa973f33f6074ee9997a8c86
Fabio Macarty <macartyfabio@gmail.com>
2023-09-10 11:51:32 -0300

    feat: add tests and create domain validation

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace Core\Domain\Entity;
 
 use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\Exception\EntityValidationException;
 
 class Category
 {
@@ -14,15 +15,38 @@ class Category
         protected string $description = '',
         protected bool $isActive = true
     ) {
+        $this->validate();
     }
 
-    public function activate(): void
+    public function activate():void
     {
        $this->isActive = true; 
     }
 
-    public function disable(): void
+    public function disable():void
     {
        $this->isActive = false; 
+    }
+
+    public function update(string $name, string $description = ''):void
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->validate();
+    }
+
+    public function validate():void
+    {
+      if(empty($this->name)){
+          throw new EntityValidationException("name is invalid");
+      }
+
+      if(strlen($this->name) > 255 || strlen($this->name) <= 2){
+        throw new EntityValidationException(' is invalid');
+      }
+      
+      if($this->description != '' && (strlen($this->description) > 255 || strlen($this->description) <= 2)){
+        throw new EntityValidationException('description is invalid');
+      }
     }
 }
--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Domain\Entity;
 
 use Core\Domain\Entity\Category;
+use Core\Domain\Exception\EntityValidationException;
 use PHPUnit\Framework\TestCase;
 
 class CategoryUnitTest extends TestCase
@@ -40,6 +41,38 @@ class CategoryUnitTest extends TestCase
         $this->assertTrue($category->isActive);
         $category->disable();
         $this->assertFalse($category->isActive);
+    }
 
+    public function testUpdate()
+    {
+        $uuid = 'uuid.value';
+
+        $category = new Category(
+            id: $uuid,
+            name: "New Category",
+            description: "New Description",
+        );
+
+        $category->update(
+            name: "New Name",
+            description: 'New Description'
+        );
+
+        $this->assertEquals('New Name', $category->name);
+        $this->assertEquals('New Description', $category->description);
+    }
+
+
+    public function testException()
+    {
+        try {
+            $category = new Category(
+                name: 'Ne',
+                description: 'New Desc'
+            );
+            $this->assertTrue(false);
+        } catch (\Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+        }
     }
 }
